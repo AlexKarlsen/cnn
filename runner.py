c3ee43181f0e17d203b4ec0e434873e09ef63c88
ajk@mjolner.dk <ajk@mjolner.dk>
2019-04-25 20:57:45 +0200

    less gpu usage

--- runner.py
+++ runner.py
@@ -6,7 +6,7 @@ import time
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.per_process_gpu_memory_fraction = 0.3
 config.gpu_options.allow_growth=True
 set_session(tf.Session(config=config))
 
