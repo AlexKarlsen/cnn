09b44c3c1ec09858b48ec0171c22056fb9d641f8
ajk@mjolner.dk <ajk@mjolner.dk>
2019-04-30 11:13:24 +0200

    more dropout

--- neural_nets/inceptionv3.py
+++ neural_nets/inceptionv3.py
@@ -32,7 +32,7 @@ class inceptionv3(nn_base):
             self.base,
             #Dropout(rate=0.3),
             Dense(1024, activation='relu'),
-            Dropout(rate=0.6),
+            Dropout(rate=0.8),
             Dense(self.n_classes, activation="softmax")
         ])
 
